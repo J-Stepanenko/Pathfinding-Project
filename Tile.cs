81147c37123a904a57beca7d6bcb1b30644812db
Dwarfskii <insaneguy256@hotmail.com>
2026-02-13 10:13:05 +0000

    .

--- Tile.cs
+++ Tile.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Tile : Node2D
 {
+	private Area2D area;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var meshInstance = this.GetChild<MeshInstance2D>(0);
+		area = meshInstance.GetChild<Area2D>(0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+	}
+
+	public bool _on_area_2d_input_event()
+	{
+
 	}
 }
